91d70cfc8bef502d087b3bf681130d78a680aa39
Mathieu MORGAT <morgatmathieu@gmail.com>
2023-03-12 23:05:03 +0100

    removed old index, and added JQuery

--- index.php
+++ index.php
@@ -3,12 +3,11 @@ require_once('main/header.php');
 require_once('main/navbar.php');
 
 if(isset($_GET['page']) == false){
-    require_once('accueil');
+    require_once('main/accueil.php');
 }
 else if($_GET['page'] == 'accueil'){
-    require_once('accueil');
+    require_once('main/accueil.php');
 }
-
 
 require_once('main/footer.php');
 ?>
--- main/header.php
+++ main/header.php
@@ -10,6 +10,9 @@
         <meta http-equiv="expires" content="Tue, 01 Jan 1980 1:00:00 GMT" />
         <meta http-equiv="pragma" content="no-cache" />
 
+        <script src="https://code.jquery.com/jquery-3.6.0.js"
+                integrity="sha256-H+K7U5CnXl1h5ywQfKtSj8PCmoN9aaq30gDh27Xc0jk=" crossorigin="anonymous"></script>
+
         <!-- Bootstrap CSS -->
         <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-GLhlTQ8iRABdZLl6O3oVMWSktQOp6b7In1Zl3/Jr59b6EGGoI1aFkw7cmDA6j6gD" crossorigin="anonymous">
         <!-- CSS personnel !-->
