34f77cc2e4b3abfab0695398f2499a70132e8e50
Mathieu MORGAT <morgatmathieu@gmail.com>
2023-03-12 17:10:33 +0100

    moved navbar to nav.php

--- index.php
+++ index.php
@@ -1,12 +1,14 @@
 <?php
 require_once('main/header.php');
+require_once('main/navbar.php');
 
 if(isset($_GET['page']) == false){
-    
-}
-if($_GET['page'] == 'accueil'){
     require_once('accueil');
 }
+else if($_GET['page'] == 'accueil'){
+    require_once('accueil');
+}
+
 
 require_once('main/footer.php');
 ?>
